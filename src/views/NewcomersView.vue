c979620edc6c176c532672b810f0d47e90f7dfd0
Bowen Tian <herebowen@gmail.com>
2025-04-08 10:13:45 +1000

    fix: resolve error

--- src/views/NewcomersView.vue
+++ src/views/NewcomersView.vue
@@ -18,7 +18,7 @@
 
   <div class="section" data-aos="fade-up">
     <div class="image-container">
-      <img src="../assets/support-services.jpg" alt="To place a imagine" class="image" />
+      <img src="../assets/welcome.jpg" alt="Newcomers to Victoria" class="image" />
     </div>
     <h2>2</h2>
     <ul>
@@ -145,7 +145,7 @@ import { onUnmounted } from 'vue';
 <style scoped>
 .section {
   max-width: 1000px;
-  margin: 80px auto 60px;
+  margin: 60px auto;
   padding: 50px 40px;
   border-radius: 16px;
   box-shadow: 0 8px 24px rgba(0, 0, 0, 0.08);
