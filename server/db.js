e7517fbe3bdbfd7a89d957d15a7c6944f842d5cf
SejarTang <stan0275@student.monash.edu>
2025-04-10 04:20:55 +1000

    content: optimize auto deployment

--- server/db.js
+++ server/db.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'ie-project-db.c5ewi2w0m89v.ap-southeast-2.rds.amazonaws.com',
   user: 'immi_project',
-  password: 'immi_project',
+  host: 'ie-project-db.c5ewi2w0m89v.ap-southeast-2.rds.amazonaws.com',
   database: 'postgres',
+  password: 'immi_project',
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
