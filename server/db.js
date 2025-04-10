630c33590427840f33fe82b6f9ca01a5ff1cabc7
Bowen Tian <herebowen@gmail.com>
2025-04-10 22:33:35 +1000

    fix: db info fix

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'immi_project',
+  user: 'projectadmindb',
   host: 'ie-project-db.c5ewi2w0m89v.ap-southeast-2.rds.amazonaws.com',
   database: 'postgres',
-  password: 'immi_project',
+  password: 'immiprojdb',
   port: 5432,
   ssl: {
     rejectUnauthorized: false
