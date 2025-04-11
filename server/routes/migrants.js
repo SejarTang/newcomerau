45c4051b2d7ba91d94e760ef8d74a6ccd889074d
jugalnayal-stack <nayal.jugal@outlook.com>
2025-04-11 16:42:36 +1000

    deployment: backend test

--- server/routes/migrants.js
+++ server/routes/migrants.js
@@ -7,7 +7,7 @@ router.get('/migrants', async (req, res) => {
     const result = await pool.query('SELECT * FROM migrants');
     res.json(result.rows);
   } catch (err) {
-    console.error('Migrants Error:', err);
+    console.error('Migrants API Error:', err);
     res.status(500).json({ error: err.message });
   }
 });
