56f4f58c9c21f18ea976606e4109629d1885680c
SejarTang <feasacht.sy@gmail.com>
2025-04-29 13:35:06 +1000

    fix: solve some bugs for backend deployment

--- server/routes/map.js
+++ server/routes/map.js
@@ -6,7 +6,7 @@ const db = require('../db');
 // get map locations
 router.get('/map-locations', async (req, res) => {
   try {
-    // clinics
+    // Clinics 
     const clinicsQuery = `
       SELECT c.clinic_code, c.clinic_type, c.clinic_name, c.clinic_address, c.suburb, c.post_code, c.phone, c.opening_hours, c.rating, c.service,
              g.latitude, g.longitude
@@ -15,7 +15,7 @@ router.get('/map-locations', async (req, res) => {
     `;
     const clinics = await db.query(clinicsQuery);
 
-    // hospitals
+    // Hospitals
     const hospitalsQuery = `
       SELECT h.hos_code, h.hos_type, h.hos_state, h.hos_name, h.suburb, h.postcode, h.second_tier_eligible, h.second_tier_category,
              h.hos_address, h.phone, h.opening_hours, h.rating, h.service,
@@ -25,7 +25,7 @@ router.get('/map-locations', async (req, res) => {
     `;
     const hospitals = await db.query(hospitalsQuery);
 
-    // hospital_type type
+    // Hospital_type type
     const privateHospitals = hospitals.rows.filter(h => h.hos_type === 'Private Hospital');
     const publicHospitals = hospitals.rows.filter(h => h.hos_type === 'Public Hospital');
 
