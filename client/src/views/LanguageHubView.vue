496999b526dcc77e977b98b355df478f15c455f4
Bowen Tian <herebowen@gmail.com>
2025-04-30 19:33:23 +1000

    style: adjust the content sequence of this page

--- client/src/views/LanguageHubView.vue
+++ client/src/views/LanguageHubView.vue
@@ -2,23 +2,13 @@
   <div>
     <h2>Pronunciation Correction Demo</h2>
     <SpeakingView />
-    <ReadList />
     <OCR />
+    <ReadList />
   </div>
 </template>
 
-<script>
-import SpeakingView from '@/components/SpeakingView.vue';
-import ReadList from '@/components/ReadList.vue';
-import OCR from '@/components/UploadOCR.vue';
-
-export default {
-components: {
-  ReadList,
-  SpeakingView,
-  OCR
-},
-};
-
-
+<script setup>
+import SpeakingView from '@/components/SpeakingView.vue'
+import ReadList from '@/components/ReadList.vue'
+import OCR from '@/components/UploadOCR.vue'
 </script>
