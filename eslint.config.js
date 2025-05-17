fc28a38490852e0b4c4fb01d9b8096c06c324047
JugaL <nayal.jugal@outlook.com>
2025-05-17 19:20:40 +1000

    security: eslint security scan

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginSecurity from 'eslint-plugin-security'
 
 export default defineConfig([
   {
@@ -11,22 +12,34 @@ export default defineConfig([
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**']),
 
   {
     languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node,
       },
     },
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
+
+  {
+    name: 'security-rules',
+    plugins: {
+      security: pluginSecurity,
+    },
+    rules: {
+      ...pluginSecurity.configs.recommended.rules,
+    },
+  },
+
   skipFormatting,
 ])
